9ab9fdbca93b89c131f01d41673221f9809d5d25
Alauddin55 <202000512@eastdelta.edu.bd>
2022-02-05 19:40:56 +0600

    change in arithmetic

--- Arithmetic12.java
+++ Arithmetic12.java
@@ -3,7 +3,7 @@ public class Arithmetic12 {
 public static void main(String[] args) {
     Scanner input= new Scanner(System.in);
     int n1,n2,rsl;
-     System.out.print("Enter a number: ");
+     System.out.print("Enter an int: ");
       n1=input.nextInt();
      System.out.print("Enter 2nd number: ");
       n2=input.nextInt();
